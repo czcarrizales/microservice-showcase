9a9a8f3816efe7722fb9760d51cd7527edba7cb8
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-06 20:20:50 -0700

    adding routes to app

--- microservice-showcase/src/App.tsx
+++ microservice-showcase/src/App.tsx
@@ -2,16 +2,26 @@ import { useState } from 'react'
 import './App.css'
 import Timestamp from './Timestamp'
 import RequestHeaderParser from './RequestHeaderParser'
+import UrlShortener from './UrlShortener'
+import ExerciseTracker from './ExerciseTracker'
+import FileMetadata from './FileMetadata'
+import {Route, Routes} from 'react-router-dom'
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
 
   return (
     <>
-    <ul>
-      <li><a href='#'>Timestamp</a></li>
-    </ul>
-      <Timestamp />
-      <RequestHeaderParser />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/timestamp' element={<Timestamp />}></Route>
+        <Route path='/requestheaderparser' element={<RequestHeaderParser />} />
+        <Route path='/urlshortener' element={<UrlShortener />} />
+        <Route path='/exercisetracker' element={<ExerciseTracker />} />
+        <Route path='/filemetadata' element={<FileMetadata />} />
+      </Routes>
     </>
   )
 }
