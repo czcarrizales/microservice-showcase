880f1fc2647de4002a450c4119a1122b5ad7a9c1
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-06 20:20:27 -0700

    request headers component

--- microservice-showcase/src/RequestHeaderParser.tsx
+++ microservice-showcase/src/RequestHeaderParser.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import axios from 'axios'
+import {useState, useEffect} from 'react'
 
 const RequestHeaderParser = () => {
+    const [data, setData] = useState()
+    const requestHeaderParser = () => {
+        axios.get('http://localhost:5000/header-parser')
+        .then((res) => {
+            console.log(res.data)
+            setData(res.data)
+        })
+    }
   return (
-    <div>RequestHeaderParser</div>
+    <div className="container">
+        <h2>Request Header Parser Microservice</h2>
+        <p>Get information about your headers!</p>
+        <button onClick={requestHeaderParser}>Get Header</button>
+        <div>
+            <p>IP ADDRESS: {data?.ipaddress}</p>
+            <p>SOFTWARE: {data?.software}</p>
+            <p>DATE: {data?.date}</p>
+            <p>HOST: {data?.host}</p>
+            
+        </div>
+      </div>
   )
 }
 
